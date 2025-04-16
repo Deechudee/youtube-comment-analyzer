b3bbb74e7a064736654420aa9a2fe00b64c6c4e8
Deechudee <deekshithadeechu14@gmail.com>
2025-04-16 10:51:58 +0530

    changes

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-# Load API key from .env file
+
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
-# Load Naive Bayes model (optional)
+
 load_naive_bayes_model()
 
 
@@ -24,18 +24,18 @@ def home():
 def analyze():
     """Handle YouTube video sentiment analysis."""
     try:
-        # Get the topic and sentiment method from the form data
+        
         topic = request.form.get("topic")
         use_naive_bayes = (
             request.form.get("use_naive_bayes") == "on"
-        )  # Checkbox for Naive Bayes
+        )  
 
         if not topic:
             return render_template(
                 "index.html", error="Please provide a topic to search for."
             )
 
-        # Fetch top YouTube videos related to the topic
+        
         videos = fetch_top_videos(API_KEY, topic)
 
         if not videos:
@@ -43,7 +43,7 @@ def analyze():
                 "index.html", error="No videos found for this topic."
             )
 
-        # Analyze sentiments for the fetched videos
+        
         analysis_results = [
             analyze_video_sentiments(video, use_naive_bayes) for video in videos
         ]
